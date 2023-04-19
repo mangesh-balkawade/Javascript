0d4d016e4a9454645478e522ef97a40bba1d1b1b
intern17 <intern17techcurve@gmail.com>
2023-04-19 21:09:43 +0530

    'DeleteUnwantedFolders'

--- Node/Middlewares/middleware.js
+++
@@ -1,7 +0,0 @@
-const middlewares = (req, res, next) => {
-  const currentDate = new Date().toISOString().slice(0, 10);
-  console.log("Api is calling on time ", currentDate);
-  next();
-};
-
-module.exports = middlewares;
